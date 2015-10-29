d28702ac304f524300da87698a070917f57fe5b5
Rostou <jrostou@gmail.com>
2015-10-29 23:49:16 +0000

    Get first picture from album
    Gets the first picture from album
    
    TODO - cycle all photos in album an get each one

---
+++ mod-graph-api/getPics.js
@@ -0,0 +1,30 @@
+var FB = require('fb');
+
+var access_token = 'CAACEdEose0cBAJ35bZAWZCXgBVdm8QVZBVfSPilwCr2uDoaS3KdLgUmaiLq0WyQG4GhM9ZAVqFYAZBJM551f8jffkhTUz3VpAOJjq9ZBHtNpcOAH7uQIBY8lVimfcfUSb8fAbIEnALB91CUZAOUtMVTDG5QXggrSnwigDi2RpHWgCVCc85ZCYdxriZClxXrxMip0xW8OKxfCL9TwxA0OXCxbY';
+
+FB.setAccessToken(access_token);
+
+FB.api('/me', 'get', {"fields":"albums{photos{images}}"}, function (res) {
+  if(!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+  var theUrl= res.albums.data[0].photos.data[0].images[0].source; 
+  
+  var fs = require('fs'),
+    request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download( theUrl , 'test.jpg', function(){
+  console.log('done');
+});
+  
+});
