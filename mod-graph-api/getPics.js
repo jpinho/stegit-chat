2c39ec46308ce27fa598538ea45ab13357258675
Rostou <jrostou@gmail.com>
2015-10-30 03:24:01 +0000

    Download all album pictures
    All pictures, from all albums are downloaded.

--- mod-graph-api/getPics.js
+++ mod-graph-api/getPics.js
@@ -1,6 +1,6 @@
 var FB = require('fb');
 
-var access_token = 'CAACEdEose0cBAJ35bZAWZCXgBVdm8QVZBVfSPilwCr2uDoaS3KdLgUmaiLq0WyQG4GhM9ZAVqFYAZBJM551f8jffkhTUz3VpAOJjq9ZBHtNpcOAH7uQIBY8lVimfcfUSb8fAbIEnALB91CUZAOUtMVTDG5QXggrSnwigDi2RpHWgCVCc85ZCYdxriZClxXrxMip0xW8OKxfCL9TwxA0OXCxbY';
+var access_token = 'CAACEdEose0cBAORBRLFeltBf4cvmBYtULiOorUyiFnf9YDSHxH76SHgiZAOUILCJFLfQY1zhPc4hSk02zv0hWR7BSRPly2WZAq8VnqnZA9Pv6ZBo2ZAtkjy5g6bRtjPIiULM2uMine5Wiul6OPdV11cQ8u51AsYtsV4qMllFnPXxTNYQlkj6SwA3b6Xv9hBcGZADgdpFykVAZDZD';
 
 FB.setAccessToken(access_token);
 
@@ -9,22 +9,51 @@ FB.api('/me', 'get', {"fields":"albums{photos{images}}"}, function (res) {
     console.log(!res ? 'error occurred' : res.error);
     return;
   }
-  var theUrl= res.albums.data[0].photos.data[0].images[0].source; 
+    
+  var albumIndex;
+  var albumsPhotoList = [];   
+  for (albumIndex = 0; albumIndex <= res.albums.data.length-1; albumIndex++) {
+    console.log("Album "+ albumIndex + " has " + res.albums.data[albumIndex].photos.data.length + " photos");
+    albumsPhotoList[albumIndex] = res.albums.data[albumIndex].photos;
+  }
   
   var fs = require('fs'),
-    request = require('request');
+  request = require('request');
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
+  var download = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){
     console.log('content-type:', res.headers['content-type']);
     console.log('content-length:', res.headers['content-length']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
+    });
+  };
 
-download( theUrl , 'albums/test.jpg', function(){
-  console.log('done');
-});
-  
+  var currentPhotoIndex;
+  var photoCount = 0;
+  var albumCount=0;
+  var currentAlbum = [];
+  var albumPhotoIndex;
+  for (albumPhotoIndex = 0; albumPhotoIndex < albumsPhotoList.length; albumPhotoIndex++){
+	console.log("Extracting photos of album " + albumPhotoIndex);
+	currentAlbum = albumsPhotoList[albumPhotoIndex].data;	
+	for (currentPhotoIndex = 0; currentPhotoIndex < currentAlbum.length; currentPhotoIndex++) {
+		console.log("Photo " + currentPhotoIndex +" of album " + albumPhotoIndex + " is being downloaded");
+		var currentPhotoURL = currentAlbum[currentPhotoIndex].images[0].source;	
+			if(albumCount != albumsPhotoList.length -1){
+				download( currentPhotoURL , 'downloadedPics/Album_'+ albumPhotoIndex + '-Photo ' + currentPhotoIndex +'.jpg', function(){
+				photoCount++;
+				console.log('Photo ' + photoCount + ' from album ' + albumPhotoIndex + ' downloaded.');	
+				});
+			}else{
+				download( currentPhotoURL , 'downloadedPics/Profile_photo ' + currentPhotoIndex +'.jpg', function(){
+				photoCount++;
+				console.log('Profile photo ' + currentPhotoIndex + ' downloaded.');			
+				});
+			} 					
+	}
+	if(photoCount = currentAlbum.length){
+			console.log('Done: All photos from album ' + albumPhotoIndex +' have been downloaded.');
+			photoCount=0;
+	}	
+   }
 });
